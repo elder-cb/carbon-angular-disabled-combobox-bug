49dd8e96d02a6d9fcd307ed58f1684666521b614
Claudio Bertoli <elder-cb@users.noreply.github.com>
2024-07-24 11:46:38 +0100

    Add disabled combobox to show bug

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,39 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ComboBoxModule, GridModule, ListItem } from 'carbon-components-angular';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule, GridModule, FormsModule, ReactiveFormsModule, ComboBoxModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'carbon-angular-disabled-combobox-bug';
+export class AppComponent implements OnInit {
+  title = 'Carbon angular: disabled combobox bug';
+  formGroup: FormGroup
+  listItems: ListItem[] = []
+
+  constructor() {
+    this.formGroup = new FormGroup({
+      combobox: new FormControl()
+    })
+  }
+
+  get comboboxControl(): FormControl {
+    return this.formGroup.controls['combobox'] as FormControl
+  }
+
+  ngOnInit(): void {
+    this.listItems = [
+      { content: 'Item 1', selected: false },
+      { content: 'Item 2', selected: false },
+      { content: 'Item 3', selected: true },
+      { content: 'Item 4', selected: false },
+      { content: 'Item 5', selected: false }
+    ]
+    this.comboboxControl.setValue('Item 3')
+    this.comboboxControl.disable()
+  }
 }
